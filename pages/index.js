2fa9260fd4b2655b54d96e9d58f429fe61be2ce9
Jonatas Faria <jonatas.martins@zig.fun>
2025-06-07 09:27:52 -0300

    .

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <h1>Jonatas</h1>;
-}
-
-export default Home;
--- tests/integration/api/v1/status/get.test.js
+++
@@ -1,8 +0,0 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-  expect(response.status).toBe(200);
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.maxConnections).toEqual("100");
-});
